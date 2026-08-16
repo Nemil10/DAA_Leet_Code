da3360d99d30fa7436ad6ce1182f348fbf8e80de
Nemil10 <nemilchhagani@gmail.com>
2026-08-16 23:11:19 +0530

    Time: 12 ms (9.63%), Space: 16.5 MB (20.45%) - LeetHub

---
+++ 0932-beautiful-array/0932-beautiful-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> beautifulArray(int n) {
+
+        if(n == 1)
+            return {1};
+
+        vector<int> ans;
+
+        vector<int> a = beautifulArray((n + 1) / 2);
+        vector<int> b = beautifulArray(n / 2);
+
+        for(int x : a)
+        {
+            ans.push_back(2 * x - 1);
+        }
+
+        for(int x : b)
+        {
+            ans.push_back(2 * x);
+        }
+
+        return ans;
+    }
+};
